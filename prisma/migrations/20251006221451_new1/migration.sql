86da51ca8c27d6014a9528bc252258965fefa1d3
LucianoMazar <lmazaraojr@gmail.com>
2025-10-06 19:34:08 -0300

    tentativa de correção migrates

--- prisma/migrations/20251006221451_new1/migration.sql
+++ prisma/migrations/20251006221451_new1/migration.sql
@@ -1,3 +1,6 @@
+-- CreateEnum
+CREATE TYPE "StatusCandidaturas" AS ENUM ('PENDENTE', 'APROVADO', 'RECUSADO');
+
 -- CreateTable
 CREATE TABLE "Endereco" (
     "id" SERIAL NOT NULL,
@@ -15,18 +18,21 @@ CREATE TABLE "Endereco" (
 -- CreateTable
 CREATE TABLE "Empresa" (
     "id" SERIAL NOT NULL,
-    "nome" TEXT NOT NULL,
     "razaoSocial" TEXT NOT NULL,
     "nomeFantasia" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "senha" TEXT NOT NULL,
-    "cnpj" TEXT,
+    "cnpj" TEXT NOT NULL,
     "telefoneComercial" TEXT NOT NULL,
+    "numFunc" INTEGER NOT NULL,
+    "numFuncPcd" INTEGER NOT NULL,
+    "area" TEXT NOT NULL,
     "site" TEXT,
     "descricao" TEXT,
     "historia" TEXT,
     "missao" TEXT,
     "valores" TEXT,
+    "foto" BYTEA,
     "certificacoes" TEXT[],
     "enderecoId" INTEGER,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -46,6 +52,9 @@ CREATE TABLE "Candidato" (
     "sexo" TEXT,
     "genero" TEXT,
     "telefones" TEXT[],
+    "foto" BYTEA,
+    "laudo" BYTEA NOT NULL,
+    "areaInteresse" TEXT NOT NULL,
     "enderecoId" INTEGER,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
@@ -103,7 +112,8 @@ CREATE TABLE "SubtipoBarreira" (
 -- CreateTable
 CREATE TABLE "Acessibilidade" (
     "id" SERIAL NOT NULL,
-    "descricao" TEXT NOT NULL,
+    "nome" TEXT NOT NULL,
+    "empresaId" INTEGER NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
@@ -119,7 +129,7 @@ CREATE TABLE "BarreiraAcessibilidade" (
 );
 
 -- CreateTable
-CREATE TABLE "JobPosition" (
+CREATE TABLE "Vagas" (
     "id" SERIAL NOT NULL,
     "titulo" TEXT NOT NULL,
     "descricao" TEXT NOT NULL,
@@ -127,38 +137,69 @@ CREATE TABLE "JobPosition" (
     "salario" DOUBLE PRECISION,
     "modalidade" TEXT,
     "cargaHoraria" TEXT,
+    "status" TEXT NOT NULL DEFAULT 'DISPONIVEL',
+    "habilidades" TEXT[],
+    "apoios" TEXT[],
     "empresaId" INTEGER NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "JobPosition_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "Vagas_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "Application" (
+CREATE TABLE "Candidaturas" (
     "id" SERIAL NOT NULL,
     "candidatoId" INTEGER NOT NULL,
     "vagaId" INTEGER NOT NULL,
-    "status" TEXT NOT NULL DEFAULT 'PENDENTE',
+    "status" "StatusCandidaturas" NOT NULL DEFAULT 'PENDENTE',
     "dataCandidatura" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
-    CONSTRAINT "Application_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "Candidaturas_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "FormacaoOuCurso" (
     "id" SERIAL NOT NULL,
     "candidatoId" INTEGER NOT NULL,
-    "nome" TEXT NOT NULL,
+    "nomeCurso" TEXT NOT NULL,
+    "tipoFormacao" TEXT NOT NULL,
     "instituicao" TEXT NOT NULL,
-    "nivel" TEXT NOT NULL,
-    "dataInicio" TIMESTAMP(3),
-    "dataFim" TIMESTAMP(3),
+    "situacao" TEXT NOT NULL,
+    "dataInicio" TIMESTAMP(3) NOT NULL,
+    "dataFim" TIMESTAMP(3) NOT NULL,
     "descricao" TEXT,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "FormacaoOuCurso_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Habilidades" (
+    "id" SERIAL NOT NULL,
+    "candidatoId" INTEGER NOT NULL,
+    "nome" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Habilidades_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Experiencias" (
+    "id" SERIAL NOT NULL,
+    "candidatoId" INTEGER NOT NULL,
+    "titulo" TEXT NOT NULL,
+    "instituicao" TEXT NOT NULL,
+    "dataInicio" TIMESTAMP(3) NOT NULL,
+    "dataFim" TIMESTAMP(3) NOT NULL,
+    "descricao" TEXT,
+    "tipoContrato" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Experiencias_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
@@ -192,13 +233,13 @@ CREATE UNIQUE INDEX "Barreira_descricao_key" ON "Barreira"("descricao");
 CREATE INDEX "SubtipoBarreira_barreiraId_idx" ON "SubtipoBarreira"("barreiraId");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Acessibilidade_descricao_key" ON "Acessibilidade"("descricao");
+CREATE UNIQUE INDEX "Acessibilidade_nome_key" ON "Acessibilidade"("nome");
 
 -- CreateIndex
 CREATE INDEX "BarreiraAcessibilidade_acessibilidadeId_idx" ON "BarreiraAcessibilidade"("acessibilidadeId");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Application_candidatoId_vagaId_key" ON "Application"("candidatoId", "vagaId");
+CREATE UNIQUE INDEX "Candidaturas_candidatoId_vagaId_key" ON "Candidaturas"("candidatoId", "vagaId");
 
 -- AddForeignKey
 ALTER TABLE "Empresa" ADD CONSTRAINT "Empresa_enderecoId_fkey" FOREIGN KEY ("enderecoId") REFERENCES "Endereco"("id") ON DELETE SET NULL ON UPDATE CASCADE;
@@ -222,19 +263,28 @@ ALTER TABLE "SubtipoBarreira" ADD CONSTRAINT "SubtipoBarreira_subtipoId_fkey" FO
 ALTER TABLE "SubtipoBarreira" ADD CONSTRAINT "SubtipoBarreira_barreiraId_fkey" FOREIGN KEY ("barreiraId") REFERENCES "Barreira"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
+ALTER TABLE "Acessibilidade" ADD CONSTRAINT "Acessibilidade_empresaId_fkey" FOREIGN KEY ("empresaId") REFERENCES "Empresa"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
 ALTER TABLE "BarreiraAcessibilidade" ADD CONSTRAINT "BarreiraAcessibilidade_barreiraId_fkey" FOREIGN KEY ("barreiraId") REFERENCES "Barreira"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "BarreiraAcessibilidade" ADD CONSTRAINT "BarreiraAcessibilidade_acessibilidadeId_fkey" FOREIGN KEY ("acessibilidadeId") REFERENCES "Acessibilidade"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "JobPosition" ADD CONSTRAINT "JobPosition_empresaId_fkey" FOREIGN KEY ("empresaId") REFERENCES "Empresa"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "Vagas" ADD CONSTRAINT "Vagas_empresaId_fkey" FOREIGN KEY ("empresaId") REFERENCES "Empresa"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Application" ADD CONSTRAINT "Application_candidatoId_fkey" FOREIGN KEY ("candidatoId") REFERENCES "Candidato"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "Candidaturas" ADD CONSTRAINT "Candidaturas_candidatoId_fkey" FOREIGN KEY ("candidatoId") REFERENCES "Candidato"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Application" ADD CONSTRAINT "Application_vagaId_fkey" FOREIGN KEY ("vagaId") REFERENCES "JobPosition"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "Candidaturas" ADD CONSTRAINT "Candidaturas_vagaId_fkey" FOREIGN KEY ("vagaId") REFERENCES "Vagas"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "FormacaoOuCurso" ADD CONSTRAINT "FormacaoOuCurso_candidatoId_fkey" FOREIGN KEY ("candidatoId") REFERENCES "Candidato"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Habilidades" ADD CONSTRAINT "Habilidades_candidatoId_fkey" FOREIGN KEY ("candidatoId") REFERENCES "Candidato"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Experiencias" ADD CONSTRAINT "Experiencias_candidatoId_fkey" FOREIGN KEY ("candidatoId") REFERENCES "Candidato"("id") ON DELETE CASCADE ON UPDATE CASCADE;
