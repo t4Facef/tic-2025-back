f37a7f072921db41085fe745e9091dcd9dc2e144
LucianoMazar <lmazaraojr@gmail.com>
2025-10-21 10:22:40 -0300

    fix: retirando dist do github

--- dist/routes/candidato-subtipo.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const candidato_subtipo_controller_1 = require("../controllers/candidato-subtipo.controller");
-const router = (0, express_1.Router)();
-router.get("/candidato/:candidatoId", candidato_subtipo_controller_1.CandidatoSubtipoController.getByCandidato);
-router.post("/", candidato_subtipo_controller_1.CandidatoSubtipoController.vincular);
-router.delete("/", candidato_subtipo_controller_1.CandidatoSubtipoController.desvincular);
-exports.default = router;
