d406b637d23dc592b8112a40d2ed0bbb59262b2b
LucianoMazar <lmazaraojr@gmail.com>
2025-11-08 11:00:47 -0300

    fix: correção de erros

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,7 @@ const app = express();
 const prisma = new PrismaClient();
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: ['http://localhost:5173', 'http://localhost:3000'],
   credentials: true
 }));
 app.use(express.json());
